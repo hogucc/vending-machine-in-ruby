143141c9e0c3c982f53f95b5cd05e3ff007c1281
hogucc <uku.h1r8@gmail.com>
2020-06-21 18:52:22 +0900

    step0のテストを追加

--- test/suica_test.rb
+++ test/suica_test.rb
@@ -2,16 +2,21 @@ require 'minitest/autorun'
 require './lib/suica'
 
 class SuicaTest < Minitest::Test
-  def test_step_0_Suicaのチャージ
-    # 以下の2行は動作確認用のコードなので削除して良い
-    suica = Suica.new
-    assert !suica.nil?
-
-    # 以下の要求仕様（テストケース）は必要に応じてテストメソッド（def test_xxx）を分けても良い
-    #
-    # Suicaには100円以上の任意の金額をチャージできる
-    # Suicaは現在のチャージ残高を取得できる。
+  def setup
+    @suica = Suica.new(charge: 1000, age: 10, sex: "男性")
   end
 
-  # 以下、step1以降の要求仕様も同様にTDDで自動販売機プログラムを書いていく
+  def test_step_0_succeeded_charge
+    assert_equal 1100, @suica.charge(100)
+  end
+
+  def test_step_0_failed_charge
+    assert_raises(StandardError) do
+      @suica.charge(99)
+    end
+  end
+
+  def test_step_0_get_charged_money_amount
+    assert_equal 1000, @suica.charged_money_amount
+  end
 end
