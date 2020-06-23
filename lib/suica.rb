5bb6c32ac462649e6918eb6e1564c3793de84158
hogucc <uku.h1r8@gmail.com>
2020-06-23 19:44:02 +0900

    suica.rbに改行を追加、payable?メソッドを追加

--- lib/suica.rb
+++ lib/suica.rb
@@ -2,8 +2,10 @@
 
 class Suica
   MINIMUM_CHARGE_MONEY = 100
+
   attr_reader :charged_money_amount, :user_age, :user_sex, :bought_time
-  def initialize(charge: 0, age: 0, sex: "男性")
+
+  def initialize(charge: 0, age: nil, sex: nil)
     @charged_money_amount = charge
     @user_age = age
     @user_sex = sex
@@ -11,10 +13,16 @@ class Suica
 
   def charge(money)
     return nil if money < MINIMUM_CHARGE_MONEY
+
     @charged_money_amount += money
   end
 
   def pay(money)
+    raise "残高が足りません" unless self.payable?(money)
     @charged_money_amount -= money
+  end
+
+  def payable?(money)
+    @charged_money_amount >= money
   end
 end
